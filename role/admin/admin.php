ee4c8f1ca208019fdf65eb438cdb4c5c293557c0
DProtazyuk <dprotazyuk@yandex.ua>
2022-02-10 20:07:24 +0300

    e

--- role/admin/admin.php
+++ role/admin/admin.php
@@ -3,6 +3,22 @@
     if(!isset($_COOKIE['pass_admin'])) {
         header("location: ../../index.php");
     }
-    else {
-        echo "yes";
-    }
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">    <link rel="stylesheet" href="/style/index.css">
+    <link rel="stylesheet" href="../../style/admin.css">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
