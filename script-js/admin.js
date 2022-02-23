4e3810ed7c0701bdb7df8b58d21a85327d885184
poilkjmnb564329 <dprotazyuk@yandex.ua>
2022-02-23 20:05:21 +0200

    asas

--- script-js/admin.js
+++ script-js/admin.js
@@ -19,42 +19,4 @@ $('.insert_prepod').on('click', function() {
     });
 });
 
-// $('#select_prepod').on('change', function() {
-//     var id_prepod =  $('#select_prepod').val();
-//
-//     $.ajax({
-//         url: 'select.php',
-//         method: 'post',
-//         ddataType: 'html',
-//         data: {id_prepod: id_prepod},
-//         success: function(data){
-//
-//             var wer = JSON.parse(data);
-//
-//             document.getElementById("update_login_prepod").value = "";
-//             document.getElementById('update_login_prepod').value = wer[0][1];
-//
-//             document.getElementById("update_pass_prepod").value = "";
-//             document.getElementById('update_pass_prepod').value = wer[0][3];
-//
-//             document.getElementById("update_surname_prepod").value = "";
-//             document.getElementById('update_surname_prepod').value = wer[0][4];
-//
-//             document.getElementById("update_name_prepod").value = "";
-//             document.getElementById('update_name_prepod').value = wer[0][5];
-//
-//             document.getElementById("update_patronymic_prepod").value = "";
-//             document.getElementById('update_patronymic_prepod').value = wer[0][6];
-//
-//             document.getElementById("update_predmet_prepod").value = "";
-//             document.getElementById('update_predmet_prepod').value = wer[0][7];
-//
-//             console.log(wer);
-//         }
-//     });
-// });
-//
-//
-// $('.update_prepod_but').on('click', function() {
-//     alert("sds");
-// });
+/
